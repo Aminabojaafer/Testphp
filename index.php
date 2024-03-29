d672f8aa1ecf178512d45d07d4b5765035580c4f
Aminabojaafer <101141709+Aminabojaafer@users.noreply.github.com>
2024-03-29 17:04:57 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,252 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+
+<body><!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>10 مربعات داخل div رئيسي</title>
+<style>
+    .main-div {
+         width: 1010px;
+        
+        display: flex;
+        flex-wrap: wrap;
+        gap: 1px;
+        border:2px ;
+       text-align: center;
+        padding: 1px;
+        margin: auto;
+         
+    }
+
+    .sub-div {
+        width: 500px;
+        height: 400px;
+       
+        border: 1px solid black;
+   
+        margin: 1px;
+        
+    }
+    .img-head-div1 {
+        width: 1010px;
+        height: 100px;
+        background-color: #efefef;
+        border: 1px solid blue;
+        float: right;
+        margin: auto;
+             }
+    .img-head-div2 {
+        width: 1010px;
+        height: 100px;
+        background-color: #00788e;
+        border: 1px solid blue;
+        float: right;
+        margin: auto;
+             }
+     .img-head-div3 {
+        width: 1010px;
+        height: 75px;
+        background-color: #00788e;
+        border: 1px solid blue;
+        float: right;
+        margin: auto;
+             }
+    .img-all {
+       width: 110px;
+       height: 70px;
+       justify-content: center;      
+       display: flex;
+       text-align: center;
+    }
+    .list1{
+        width: 1010px;
+        height: 50px;
+        background-color: #beb07e;
+        border: 1px solid blue;
+        float: right;
+        margin: auto;
+             }  
+    .list2{
+        width: 1010px;
+        height: 100px;
+        background-color: #beb07e;
+        border: 1px solid blue;
+        float: right;
+        margin: auto;
+             } 
+        
+    .sub-div1 {
+        width: 200px;
+        height: 98px;
+        
+      
+      display: flex;
+    justify-content: left;
+    align-items: center;
+                 float: right;
+        
+    }
+    .sub-div2 {
+        width: 780px;
+        height: 98px;
+   
+      
+   
+        text-align: right;
+        
+                
+        
+    }
+</style>
+</head>
+<body>
+<div class="main-div">
+<div class="img-head-div1" >
+    <p>&nbsp;</p>
+    <p><strong>+966504265634 <img src="img/whatsapp.png" alt="واتساب" width="20" height="20"> تواصل معنا</strong></p>
+</div>
+  <div class="img-head-div2" ><img width="610" height="100" lang="center"  src="img/01.png"  alt=""/><strong></strong>
+  
+     
+  </div>
+    <div class="list1">
+    <h5>الرحلات السياحية ⎮ كروز الخليج ⎮ حجوزات الفنادق ⎮ متجر الهدايا</h5>
+    </div>
+<div class="sub-div"> <img src="img/02.jpg" width="499" height="350"  alt=""/>
+  <blockquote>
+    <blockquote>
+      <blockquote>
+        <blockquote>
+			<a href="https://richexperience.sa/ar/ADezzD"> <h4><strong>رحلة خاصة في الطائف</strong></h4></a>
+        </blockquote>
+      </blockquote>
+    </blockquote>
+  </blockquote>
+</div>
+  <div class="sub-div">
+<div><img src="img/03.jpg" width="490" height="350" alt=""/>
+  <blockquote>
+    <blockquote>
+      <blockquote>
+        <blockquote>
+			<a href="https://richexperience.sa/ar/ZYenEvp"><h4><strong>رخصة القيادة الدولية </strong></h4></a>
+        </blockquote>
+      </blockquote>
+    </blockquote>
+  </blockquote>
+  &nbsp;</div>
+  </div>
+    <div class="sub-div"> <img src="img/04.jpg" width="490" height="350" alt=""/>
+      <blockquote>
+        <blockquote>
+          <blockquote>
+            <blockquote>
+              <blockquote>
+               <a href="https://richexperience.sa/ar/pQKrnzp"> <h4><strong>اليخت الفاخر</strong></h4></a>
+              </blockquote>
+            </blockquote>
+          </blockquote>
+        </blockquote>
+      </blockquote>
+    </div>
+  <div class="sub-div"> <strong><img src="img/05.jpg" width="490" height="350" alt=""/></strong>
+    <blockquote>
+      <blockquote>
+        <blockquote>
+          <blockquote>
+            <a href="https://richexperience.sa/ar/XedGDrj"><h3><strong>خدمة الاستقبال بالمطار</strong></h3></a>
+          </blockquote>
+        </blockquote>
+      </blockquote>
+    </blockquote>
+  </div>
+    <div class="sub-div"> <img src="img/06.jpg" width="490" height="350" alt=""/>
+      <blockquote>
+        <blockquote>
+          <blockquote>
+            <blockquote>
+             <a href="https://richexperience.sa/ar/awopxZ"> <h4><strong>جزيرة بياضة في جدة</strong></h4></a>
+            </blockquote>
+          </blockquote>
+        </blockquote>
+      </blockquote>
+    </div>
+    <div class="sub-div"><img src="img/07.jpg" width="490" height="350" alt=""/>
+     <a href="https://richexperience.sa/ar/xApNjxj">
+     <blockquote>
+       <blockquote>
+         <blockquote>
+           <blockquote>
+             <blockquote>
+               <h4><strong>سفاري جدة</strong></h4>
+             </blockquote>
+           </blockquote>
+         </blockquote>
+       </blockquote>
+     </blockquote>
+     </a>
+    </div>
+<div class="sub-div"> <img src="img/08.jpg" width="490" height="350" alt=""/>
+  <blockquote>
+    <blockquote>
+      <blockquote>
+        <blockquote>
+          <blockquote>
+           <a href="https://richexperience.sa/ar/pepBRw"><h4><strong>جدة التاريخية</strong></h4></a>
+          </blockquote>
+        </blockquote>
+      </blockquote>
+    </blockquote>
+  </blockquote>
+</div>
+    <div class="sub-div">
+     
+    <img src="img/09.jpg" width="490" height="350" alt=""/>
+   <a href="https://richexperience.sa/ar/NplNeB">
+   <blockquote>
+     <blockquote>
+       <blockquote>
+         <blockquote>
+           <blockquote>
+             <h4><strong>جزيرة الفرسان</strong></h4>
+           </blockquote>
+         </blockquote>
+       </blockquote>
+     </blockquote>
+   </blockquote>
+   </a>
+    </div>
+<div class="list2">
+<div class="sub-div1"><img src="img/011.png" width="50" height="50" alt=""/></div>
+    <div class="sub-div2"><h4>: التجربة الثرية لسفر والسياحة  <br>مؤسسة مرخصة من وزارة التجارة ( 4032243561 ) ومرخصة من وزارة السياحة ( 73101887 )</h4></div>
+    </div>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+<div class="img-head-div3"><img src="img/010.png" width="500" height="75" alt=""/> <strong></strong></div>
+<div class="img-head-div3"><img src="img/insta.png" width="30" height="30" alt=""/><img src="img/snap.png" width="30" height="30" alt=""/><img src="img/tiktok.png" width="30" height="30" alt=""/><img src="img/twitar.png" width="30" height="30" alt=""/><img src="img/facebook.png" width="30" height="30" alt=""/>
+    <div >
+  <h6>© Rich Experience 2024</h6>
+</div>
+  </div>
+
+</div>
+</body>
+</html>
+
+</body>
+</html>
